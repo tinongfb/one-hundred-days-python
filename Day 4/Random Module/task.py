862f94a4c5cb8da8547c96cc5ea6ba87ee3a9f2f
christine.ong <tinongfb@gmail.com>
2025-06-11 13:54:52 +0800

    wording and line break updates

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,16 @@
+import random
+# import my_module
+#
+
+# print(random_integer)
+# print(my_module)
+
+# random_num = random.random()
+# print(random_num)
+
+random_integer = random.randint(0, 1)
+print(random_integer)
+if random_integer == 0:
+    print("Heads")
+else:
+    print("Tails")
