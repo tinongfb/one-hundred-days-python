ef21fad60966067d9b9871b6873cef66f3416290
christine.ong <tinongfb@gmail.com>
2025-06-11 09:50:47 +0800

    wording and line break updates

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -25,27 +25,37 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 while True:
     print("Welcome to Treasure Island.")
     print("Your mission is to find the treasure.")
-    first_choice = input("choose your fate. each hand contains an item. choose the hand with food to continue. left or right hand? ")
+    first_choice = input("choose your fate. each hand contains an item. "
+                         "choose the hand with food to continue. left or right hand? ")
     if first_choice.lower() == "left" or first_choice == "LEFT HAND":
-        second_choice = input("you are lucky. you chose the hand that has the potato\nnow you can continue the adventyah. there is now a fork on the road. choose your direction, left or right? ")
+        second_choice = input("you are lucky. you chose the hand that has the potato"
+                              "\nnow you can continue the adventyah. "
+                              "there is now a fork on the road. choose your direction, left or right? ")
         if second_choice.lower() == "right":
-           third_choice = input("wow. okay. you're lucky. now there is a more obvious fork in the road. would you go into the forest or go by the seaside? choose forest or sea ")
+           third_choice = input("wow. okay. you're lucky. now there is a more obvious fork in the road. "
+                                "would you go into the forest or go by the seaside? choose forest or sea ")
            if third_choice.lower() == "sea" or third_choice == "seaside":
-               fourth_choice = input("do you like to swim that much? you arent swimming in this game. next question: there is now a castle and you see the entrance. do you knock on the door or not? Y/N ")
+               fourth_choice = input("do you like to swim that much? you arent swimming in this game. "
+                                     "next question: there is now a castle and you see the entrance. "
+                                     "do you knock on the door or not? Y/N ")
                if fourth_choice.lower() == "y":
-                   replay = input("you chose to knock on the door. a human with manners. the owners of the castle decide to let you live and give you treasure. congratulations. \nwould you like to play again? Y/N ")
+                   replay = input("you chose to knock on the door. a human with manners. "
+                                  "the owners of the castle decide to let you live and give you treasure. "
+                                  "you beat the game, congratulations. \nwould you like to play again? Y/N ")
                    if replay.lower() == "y":
                        print("hellye")
                    else:
                        break
                else:
-                   replay = input("the owners of the castle decided to seal your fate because you lack manners. okay that's it game over. \nwould you like to play again? Y/N ")
+                   replay = input("the owners of the castle decided to seal your fate because you lack manners. "
+                                  "well, game over. \nwould you like to play again? Y/N ")
                    if replay.lower() == "y":
                        print("awyea")
                    else:
                        break
            else:
-               replay = input("you chose the dim forest. you got eaten by wild animals. game over. \nwould you like to play again? Y/N ")
+               replay = input("you chose the dim forest. you got eaten by wild animals. game over. "
+                              "\nwould you like to play again? Y/N ")
                if replay.lower() == "y":
                    print("kk")
                else:
