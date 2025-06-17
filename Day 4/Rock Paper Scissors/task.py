66aa36b2a314e2177a32098bc91b06c13d587910
christine.ong <tinongfb@gmail.com>
2025-06-17 15:42:53 +0800

    cannot catch index limit exceeded

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,24 +27,25 @@ scissors = '''
 ---.__(___)
 '''
 
-import random
+
 player_choice_index = int(input("Let's play Rock Paper Scissors..."
       "choose your hand, 0-rock, 1-paper, 2-scissors! "
       "\nType the number \n"))
-choices = [rock, paper, scissors]
-print(choices)
+
+#print(choices)
 #int_choice = int(player_choice_index)
-your_choice = choices[player_choice_index]
-print(f"you chose: {player_choice_index} \n{your_choice}")
+#your_choice = choices[player_choice_index]
 #print("your hand: " + your_choice)
 
 random_choice_enemy_index = random.randint(0, 2)
-random_choice_enemy = choices[random_choice_enemy_index]
-print(f"Enemy choice: {random_choice_enemy_index} \n{random_choice_enemy}")
+#random_choice_enemy = choices[random_choice_enemy_index]
+choices = [rock, paper, scissors]
+print(f"Enemy chose: {random_choice_enemy_index} {choices[random_choice_enemy_index]}")
+print(f"You chose: {player_choice_index} {choices[player_choice_index]}")
 
 #compare the choices' indices, 2>1, 1>0, 0>2
 if player_choice_index > 2:
-    print("please choose rock, paper, or scissors!")
+    print("\nplease choose rock, paper, or scissors!")
 elif ((player_choice_index == 2 and random_choice_enemy_index == 0) or
         (player_choice_index == 1 and random_choice_enemy_index == 2) or
         (player_choice_index == 0 and random_choice_enemy_index == 1)):
