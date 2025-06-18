693c10ba3079cdb5db5c93c0c434a87a89baab2b
christine.ong <tinongfb@gmail.com>
2025-06-18 15:47:21 +0800

    cont day7 (start)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,3 +1,5 @@
+import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
@@ -6,3 +8,27 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
+
+#TODO-1
+def rand_word_function():
+    chosen_word = random.choice(word_list)
+    return chosen_word
+
+#TODO-2
+def guess_letter_function():
+    guess = (input("Guess a letter: ")).lower()
+    return guess
+
+#TODO-3
+def guess_in_rand_word_function():
+    chosen_word = rand_word_function()
+    guess = guess_letter_function()
+    print(f"the random word is {chosen_word}")
+    print(f"the chosen letter is {guess}")
+
+    if guess in chosen_word:
+        print("Right")
+    else:
+        print("Wrong")
+
+guess_in_rand_word_function()
