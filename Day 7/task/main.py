ac6812918a2f63d4ab52998fe6f8f708c53c0d3d
christine.ong <tinongfb@gmail.com>
2025-08-08 15:46:25 +0800

    cont day7 (start)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -7,10 +7,14 @@ chosen_word = random.choice(word_list)
 print(f"the random word is '{chosen_word}'")
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("guess a letter: ")
+guess = (input("guess a letter: ")).lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
+if guess in chosen_word:
+    print("Right")
+else:
+    print("Wrong")
 
 
 # def rand_word_function():
