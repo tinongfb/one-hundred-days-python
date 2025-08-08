540592ccc4a9c8a2fd7e06890f44711528170376
christine.ong <tinongfb@gmail.com>
2025-08-08 15:59:37 +0800

    cont day7 (start)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,40 +1,20 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
-print(f"the random word is '{chosen_word}'")
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = (input("guess a letter: ")).lower()
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+guess = input("Guess a letter: ").lower()
+placeholder = "" + "_"
+print(placeholder)
 
-#BELOW:SOLUTION
-# def rand_word_function():
-#     chosen_word = random.choice(word_list)
-#     return chosen_word
-#
-# def guess_letter_function():
-#     guess = (input("Guess a letter: ")).lower()
-#     return guess
-#
-# def guess_in_rand_word_function():
-#     chosen_word = rand_word_function()
-#     guess = guess_letter_function()
-#     print(f"the random word is {chosen_word}")
-#     print(f"the chosen letter is {guess}")
-#
-#     if guess in chosen_word:
-#         print("Right")
-#     else:
-#         print("Wrong")
-#
-# guess_in_rand_word_function()
-#
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
