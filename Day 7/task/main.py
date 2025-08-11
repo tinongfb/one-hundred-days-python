3a6a7b29e86618418819d3193cbd530c0a43c393
christine.ong <tinongfb@gmail.com>
2025-08-11 09:34:44 +0800

    cont day7 (start)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -2,19 +2,22 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
+letter_count = len(chosen_word)
+print(f"chosen word '{chosen_word}' has {letter_count} letters")
+
 
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 
 guess = input("Guess a letter: ").lower()
-placeholder = "" + "_"
-print(placeholder)
+placeholder = "_" * letter_count
+#print(placeholder)
 
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-
+word = []
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
+        word.append(guess)
     else:
-        print("Wrong")
+        word.append("_")
+print("".join(word))
 
