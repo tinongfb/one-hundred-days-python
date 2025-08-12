09e7f1fafa8fcb58fe73f182e0bf8e2fbafd612b
christine.ong <tinongfb@gmail.com>
2025-08-12 15:08:57 +0800

    cont day7 (start)

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,10 @@
+#create the function
+def greet(name):
+    print(f"Hello {name}")
+    print(f"How do you do, {name}?")
+    print(f"Bye, {name}")
+#call the function (run the function)
+
+
+name = input("What is your name? ")
+greet(name)
