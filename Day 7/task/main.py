3b8d30fc9e063f6b116c10768f806e26bb414b04
christine.ong <tinongfb@gmail.com>
2025-08-12 11:05:44 +0800

    cont day7 (start)

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,113 +1,62 @@
 import random
-stages = [r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-#NOTE : ALL PAGES' SOLUTIONS ARE IN THIS PAGE
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-# TODO-1: - Use a while loop to let the user guess again.
-# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. Set 'lives' to equal 6.
+#NOTE : THE SOLUTION FOR THIS PAGE IS IN PAGE 2
+
 # TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 
-word_list = ["aardvark", "baboon", "camel"]
 lives = 6
 
+# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+from hangman_words import word_list
+from hangman_art import stages
 chosen_word = random.choice(word_list)
-letter_count = len(chosen_word)
-print(f"chosen word '{chosen_word}' has {letter_count} letters")
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
 
 game_over = False
-letters_not_in_word = []
 correct_letters = []
 
 while not game_over:
-    guess = input("Guess a letter: ").lower()
-    placeholder = "_" * letter_count
-    #print(placeholder)
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-# TODO-2: Change the for loop so that you keep the previous correct letters in display.
-# TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
-#  If lives goes down to 0 then the game should stop and it should print "You lose."
-    word = []
+    # TODO-6: - Update the code below to tell the user how many lives they have left.
+    print("****************************<???>/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
+    display = ""
 
     for letter in chosen_word:
         if letter == guess:
-            word.append(guess)
+            display += letter
             correct_letters.append(guess)
         elif letter in correct_letters:
-            word.append(letter)
+            display += letter
         else:
-            word.append("_")
+            display += "_"
 
-    if guess not in correct_letters:
-        letters_not_in_word.append(guess)
+    print("Word to guess: " + display)
+
+    # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    #  e.g. You guessed d, that's not in the word. You lose a life.
+
+    if guess not in chosen_word:
         lives -= 1
+
         if lives == 0:
             game_over = True
-            print("Game over, no more lives left.")
 
-    print("current guess: " + "".join(word))
-    #print(f"correct guesses: {correct_letters}")
-    print(f"incorrect letters: {letters_not_in_word}")
+            # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
+            print(f"***********************YOU LOSE**********************")
 
-    if "_" not in word:
+    if "_" not in display:
         game_over = True
-        print("You guessed the word! The word is: " + "".join(word))
-        
-# TODO-3: - print the ASCII art from 'stages'
-#  that corresponds to the current number of 'lives' the user has remaining.
+        print("****************************YOU WIN****************************")
+
+    # TODO-2: - Update the code below to use the stages List from the file hangman_art.py
     print(stages[lives])
