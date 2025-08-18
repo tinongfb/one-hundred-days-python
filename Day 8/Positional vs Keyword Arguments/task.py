1f3e6a4ec59c891aa7e078da8d216bc3b51f7205
christine.ong <tinongfb@gmail.com>
2025-08-18 10:40:12 +0800

    cont day7 (start)

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,4 +1,4 @@
-# Functions with input
+Functions with input
 
 def greet_with_name(name):
     print(f"Hello {name}")
@@ -6,3 +6,41 @@ def greet_with_name(name):
 
 
 greet_with_name("Jack Bauer")
+
+
+
+### this is for EXERCISE : love calculator of DAY 8 ###
+
+# true_string = "true"
+# love_string = "love"
+# true_list = []
+# love_list = []
+# letter_list = []
+# name_a = "Mariah"
+# name_b = "Britney"
+#
+#
+# def calculate_love_score():
+#     for letter in true_string:
+#         for letter_name in name_a.lower():
+#             if letter_name == letter:
+#                 true_list.append(letter)
+#         for letter_name in name_b.lower():
+#             if letter_name == letter:
+#                 true_list.append(letter)
+#     for letter in love_string:
+#         for letter_name in name_a.lower():
+#             if letter_name == letter:
+#                 love_list.append(letter)
+#         for letter_name in name_b.lower():
+#             if letter_name == letter:
+#                 love_list.append(letter)
+#
+#     letter_list = true_list + love_list
+#     print(f"love_list: {love_list}")
+#     print(f"true_list: {true_list}")
+#     print(f"letter_list: {letter_list}")
+#
+#
+# calculate_love_score()
+
