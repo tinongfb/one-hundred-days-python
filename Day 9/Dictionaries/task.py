1b43de38231e0ad01dfeba9c67ba3b4080c0cccb
christine.ong <tinongfb@gmail.com>
2025-08-19 15:58:24 +0800

    cont day7 (start)

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,11 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+import key
+
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again.",
+                          "New": "I add new."}
+print(programming_dictionary)
+#print(programming_dictionary["New"])
+
+for key in programming_dictionary:
+    print(key)
+    #print(programming_dictionary["Bug"])
